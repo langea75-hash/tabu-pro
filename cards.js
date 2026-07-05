8df3ee828edd9623eaf12fca56704fab03e715aa
langea75-hash <lange.a75@gmail.com>
2026-07-05 08:47:29 +0200

    Create cards.js

---
+++ cards.js
@@ -0,0 +1,52 @@
+export const cards = [
+  {
+    category: "Natur",
+    word: "Sonnenblume",
+    taboo: ["gelb", "Blume", "Garten"]
+  },
+  {
+    category: "Tiere",
+    word: "Hund",
+    taboo: ["Katze", "bellen", "Leine"]
+  },
+  {
+    category: "Essen",
+    word: "Pizza",
+    taboo: ["Käse", "Tomate", "Italien"]
+  },
+  {
+    category: "Verkehr",
+    word: "Auto",
+    taboo: ["Motor", "Reifen", "Lenkrad"]
+  },
+  {
+    category: "Sport",
+    word: "Fußball",
+    taboo: ["Ball", "Tor", "Spieler"]
+  },
+  {
+    category: "Berufe",
+    word: "Arzt",
+    taboo: ["Patient", "Krankenhaus", "Praxis"]
+  },
+  {
+    category: "Alltag",
+    word: "Telefon",
+    taboo: ["Anruf", "Handy", "Nummer"]
+  },
+  {
+    category: "Reisen",
+    word: "Hotel",
+    taboo: ["Zimmer", "Urlaub", "Rezeption"]
+  },
+  {
+    category: "Musik",
+    word: "Gitarre",
+    taboo: ["Saiten", "Instrument", "Musik"]
+  },
+  {
+    category: "Filme",
+    word: "Popcorn",
+    taboo: ["Kino", "Mais", "Snack"]
+  }
+];
