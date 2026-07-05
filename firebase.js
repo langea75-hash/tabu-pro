97443240b650e3a7605ceb31a55b474033ed4667
langea75-hash <lange.a75@gmail.com>
2026-07-05 08:41:15 +0200

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLmKBM8OoqcLAVZhIHvuF0_e0trsu8-5Q",
+  authDomain: "tabu-pro-online-7cf64.firebaseapp.com",
+  projectId: "tabu-pro-online-7cf64",
+  storageBucket: "tabu-pro-online-7cf64.firebasestorage.app",
+  messagingSenderId: "498221308101",
+  appId: "1:498221308101:web:fa6b8efea669a6d10db8ac"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
